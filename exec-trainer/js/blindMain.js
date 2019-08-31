d25742687b29474bd087ed61df9ebb6dc2ea1cd6
Moreno van Rooijen <m.r.v.rooijen@student.tue.nl>
2019-08-31 14:14:51 +0200

    Updated Execution trainer. Fully working

--- exec-trainer/js/blindMain.js
+++ exec-trainer/js/blindMain.js
@@ -1,254 +1,446 @@
-var cube_corners 	= 	["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
-var cube_edges 		= 	["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
-var normal = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
-var mirror = ["m", "i", "e", "r", "c", "l", "v", "s", "b", "p", "w", "f", "a", "u", "x", "j", "d", "h", "z", "n", "g", "k", "o", "t"];
-var edge_flips = [];
-//corners
+//corner letters
+
+var cube_edges = 				["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+var solution_edges = 		["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+var mirror_edges = 			["m", "i", "e", "r", "c", "l", "v", "s", "b", "p", "w", "f", "a", "u", "x", "j", "d", "h", "z", "n", "g", "k", "o", "t"];
+var normal =            ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+var cube_corners = 			["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+var solution_corners = 	["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+var cw_corners =				["r", "m", "i", "e", "s", "c", "l", "v", "f", "b", "p", "w", "j", "a", "u", "x", "n", "d", "h", "z", "t", "g", "k", "o"];
+
+//corners cycles
 var fc = [
-	[4, 5, 6, 7],
-	[3, 8, 21, 18],
-	[2, 11, 20, 17]
+  [4, 5, 6, 7],
+  [3, 8, 21, 18],
+  [2, 11, 20, 17]
 ];
 var bc = [
-	[12, 13, 14, 15],
-	[0, 19, 22, 9],
-	[1, 16, 23, 10]
+  [12, 13, 14, 15],
+  [0, 19, 22, 9],
+  [1, 16, 23, 10]
 ];
 var uc = [
-	[0, 1, 2, 3],
-	[12, 8, 4, 16],
-	[13, 9, 5, 17]
+  [0, 1, 2, 3],
+  [12, 8, 4, 16],
+  [13, 9, 5, 17]
 ];
 var dc = [
-	[20, 21, 22, 23],
-	[7, 11, 15, 19],
-	[6, 10, 14, 18]
+  [20, 21, 22, 23],
+  [7, 11, 15, 19],
+  [6, 10, 14, 18]
 ];
 var lc = [
-	[16, 17, 18, 19],
-	[0, 4, 20, 14],
-	[3, 7, 23, 13]
+  [16, 17, 18, 19],
+  [0, 4, 20, 14],
+  [3, 7, 23, 13]
 ];
 var rc = [
-	[8, 9, 10, 11],
-	[1, 14, 21, 5],
-	[2, 12, 22, 6]
+  [8, 9, 10, 11],
+  [1, 15, 21, 5],
+  [2, 12, 22, 6]
 ];
 //edge cycles
 var fe = [
-	[4, 5, 6, 7],
-	[2, 11, 20, 17]
+  [4, 5, 6, 7],
+  [2, 11, 20, 17]
 ];
 var be = [
-	[12, 13, 14, 15],
-	[0, 19, 22, 9]
+  [12, 13, 14, 15],
+  [0, 19, 22, 9]
 ];
 var ue = [
-	[0, 1, 2, 3],
-	[4, 16, 12, 8]
+  [0, 1, 2, 3],
+  [4, 16, 12, 8]
 ];
 var de = [
-	[20, 21, 22, 23],
-	[6, 10, 14, 18]
+  [20, 21, 22, 23],
+  [6, 10, 14, 18]
 ];
 var le = [
-	[16, 17, 18, 19],
-	[3, 7, 23, 13]
+  [16, 17, 18, 19],
+  [3, 7, 23, 13]
 ];
 var re = [
-	[8, 9, 10, 11],
-	[1, 15, 21, 5]
+  [8, 9, 10, 11],
+  [1, 15, 21, 5]
 ];
 
-function getCornerBufferPiece() {
-	return cube_corners[2];
+//Returns the color of a given piece
+function getColor(piece) {
+  var color = "";
+  switch (piece) {
+    case "a":
+    case "b":
+    case "c":
+    case "d":
+      color = "white";
+      break;
+    case "e":
+    case "f":
+    case "g":
+    case "h":
+      color = "green";
+      break;
+    case "i":
+    case "j":
+    case "k":
+    case "l":
+      color = "red";
+      break;
+    case "m":
+    case "n":
+    case "o":
+    case "p":
+      color = "blue";
+      break;
+    case "r":
+    case "s":
+    case "t":
+    case "u":
+      color = "orange";
+      break;
+    case "v":
+    case "w":
+    case "x":
+    case "z":
+      color = "yellow";
+      break;
+
+    default:
+      color = "unkown";
+  }
+  return color;
 }
 
-function getEdgeBufferPiece() {
-	return cube_edges[2];
+//returns an array of colors for each piece.
+function getCubeColor(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    newArray.push(getColor(array[i]));
+  }
+  return newArray;
+
 }
+function cleanSolution(solution){
+    solution = solution.toUpperCase();
+    solution = solution.replace(/(\w{2})/g, '$1 ');
+    return solution;
+  }
 //rotate the pieces, given a certain move;
-function rotate(move) {
-	switch (move) {
-		case "F":
-			rotatepieces(fc, fe);
-			break;
-		case "B":
-			rotatepieces(bc, be);
-			break;
-		case "R":
-			rotatepieces(rc, re);
-			break;
-		case "L":
-			rotatepieces(lc, le);
-			break;
-		case "U":
-			rotatepieces(uc, ue);
-			break;
-		case "D":
-			rotatepieces(dc, de);
-			break;
-	}
+function doMove(move) {
+  switch (move) {
+    case "F":
+      rotatePieces(fc, fe);
+      break;
+    case "B":
+      rotatePieces(bc, be);
+      break;
+    case "R":
+      rotatePieces(rc, re);
+      break;
+    case "L":
+      rotatePieces(lc, le);
+      break;
+    case "U":
+      rotatePieces(uc, ue);
+      break;
+    case "D":
+      rotatePieces(dc, de);
+      break;
+  }
 }
-//
-function rotatepieces(corners, edges) {
-	//for each array, rotate the corner pieces.
-	for (var i = 0; i < corners.length; i++) {
-		var subi = corners[i][3];
-		var sub = cube_corners[subi];
-		for (var j = 3; j > 0; j--) {
-			cube_corners[corners[i][j]] = cube_corners[corners[i][j - 1]];
-		}
-		cube_corners[corners[i][0]] = sub;
-	}
-	//for each array, rotate the edge pieces.
-	for (var i = 0; i < edges.length; i++) {
-		var subi = edges[i][3];
-		var sub = cube_edges[subi];
-		for (var j = 3; j > 0; j--) {
-			cube_edges[edges[i][j]] = cube_edges[edges[i][j - 1]];
-		}
-		cube_edges[edges[i][0]] = sub;
-	}
+//rotate the individual pieces of given corner and edge cycles
+function rotatePieces(corners, edges) {
+  //for each array, rotate the corner pieces.
+  for (var i = 0; i < corners.length; i++) {
+    var subi = corners[i][3];
+    var sub = cube_corners[subi];
+    for (var j = 3; j > 0; j--) {
+      cube_corners[corners[i][j]] = cube_corners[corners[i][j - 1]];
+    }
+    cube_corners[corners[i][0]] = sub;
+  }
+  //for each array, rotate the edge pieces.
+  for (var i = 0; i < edges.length; i++) {
+    var subi = edges[i][3];
+    var sub = cube_edges[subi];
+    for (var j = 3; j > 0; j--) {
+      cube_edges[edges[i][j]] = cube_edges[edges[i][j - 1]];
+    }
+    cube_edges[edges[i][0]] = sub;
+  }
 }
-
+//given an array of moves, execute all moves in order.
 function executeScramble(array) {
-	for (var i = 0; i < array.length; i++) {
-		var move = array[i];
-		var single_move = move.charAt(0);
-		if (move.length == 1) {
-			rotate(single_move);
-		} else if (move.charAt(1) == 2) {
-			rotate(single_move);
-			rotate(single_move);
-		} else {
-			rotate(single_move);
-			rotate(single_move);
-			rotate(single_move);
-		}
-	}
-}
+	cube_corners = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+	cube_edges = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
 
+  for (var i = 0; i < array.length; i++) {
+
+    var move = array[i];
+    var single_move = move.charAt(0);
+    if (move.length == 1) {
+      doMove(single_move);
+    } else if (move.charAt(1) == 2) {
+      doMove(single_move);
+      doMove(single_move);
+    } else {
+      doMove(single_move);
+      doMove(single_move);
+      doMove(single_move);
+    }
+  }
+}
+//returns the cube corner letters.
+function getCubeCorners() {
+  return cube_corners;
+}
+//returns the cube edge letters.
+function getCubeEdges() {
+  return cube_edges;
+}
+//returns the blind solution of the cube
 function getBlindSolution() {
-	console.log("Edge: " + getEdgeSolution());
-	console.log("Corners: " + getCornerSolution());
 }
 
-function differenceIndex(a, b) {
-	for (var i = 0; i < a.length; i++) {
-		if (a[i] != b[i]) {
-			//if buffer is flipped
-			if (b[i] == "c" || b[i] == "e") {
-				//skip
-			} else {
-				//different index
-				return i;
+function getEdgeBuffer() {
+  return solution_edges[2];
+}
+
+function getCornerBuffer() {
+  return solution_corners[0];
+}
+
+function edgesSolved() {
+  if (JSON.stringify(solution_edges) === JSON.stringify(normal)) {
+    return true;
+  }
+  return false;
+}
+function cornersSolved() {
+  if (JSON.stringify(solution_corners) === JSON.stringify(normal)) {
+    return true;
+  }
+  return false;
+}
+	function getCornerSolution(){
+    var cCycles = 0;
+	var result = "";
+  solution_corners = [...cube_corners];
+  var i = 0;
+  //First get Twists
+  var twists = getTwists();
+  var newCycle = "";
+  //Then solve rest of the cube
+  while (i < 30 && !cornersSolved()) {
+    var piece = getCornerBuffer();
+
+    i++;
+    if(piece==newCycle || piece == normal[cw_corners.indexOf(newCycle)] || piece == normal[cw_corners.indexOf(normal[cw_corners.indexOf(newCycle)])]){
+      cCycles++;
+console.log("New Cycle done: " + newCycle + " and wrong corners " + countWrongCorners());
+    if(countWrongCorners()==3){
+      twistPiece("a",true);
+      if(cornersSolved()){
+        break;
+      }
+      twistPiece("a",true);
+      if(cornersSolved()){
+        break;
+      }
+      else{
+        console.log("error");
+      }
+    }
+
+
+    }
+    if (piece == "a" || piece == "n" ||piece == "r") {
+      var piece = findWrongCorner();
+      newCycle = piece;
+			if(piece=="NONE"){
+				twistPiece("a",true);
+				if(cornersSolved()){
+					break;
+				}
+				twistPiece("a",true);
+				if(cornersSolved()){
+					break;
+				}
 			}
+		}
+      result += piece;
+      swapCorner(piece);
+
+
+    //swap piece with location it belongs;
+
+  }
+  result = cleanSolution(result);
+   if(twists.length>0){
+   result += "("+cleanSolution(twists)+")";
+   }
+   console.log("Corner cycles: " +cCycles);
+  return result;
+}
+
+function getTwists(){
+	var twists = ""
+	for(var i = 0; i< solution_corners.length; i++){
+    var piece = solution_corners[i];
+		if(piece==cw_corners[i] && !(piece == "a" || piece == "n" ||piece == "r")){
+			twists += piece + "";
+			twistPiece(piece,false);
 		}
 	}
-	return -1;
-}
-
-
-
-
-function getCornerSolution() {
-	var solution = "";
-	var buffer = getCornerBufferPiece();
-	while (buffer !== "c") {
-		solution += buffer;
-		var loc = normal.indexOf(buffer);
-		buffer = cube_corners[loc];
-	}
-	return solution;
-}
-
-function findNewBuffer(array){
-	// find first difference between normal and array;
-	for (var i = 0; i < array.length; i++) {
-		if (array[i] != normal[i]) {
-			//if buffer is flipped
-			if (array[i] === "c" || array[i] === "e") {
-				//skip
-			} else {
-				//different index
-				return normal[i];
-			}
+	for(var i = 0; i< solution_corners.length; i++){
+    var piece = solution_corners[i];
+		if(piece==normal[cw_corners.indexOf(normal[i])] && !(piece == "a" || piece == "n" ||piece == "r")){
+			twists += piece + "\'";
+			twistPiece(piece,true);
 		}
 	}
-	return "no";
-}
-function swapPiece(i,j, array){
-	var temp = array[i];
-	array[i]=array[j];
-	array[j]=temp;
-}
-function swap(loc){
-	//Index buffer
-	var indexLoc = 2;
-	var indexLocMirror = 4;
-	//Index of where the location should be
-	var target = normal.indexOf(loc);
-	var targetMirror = mirror.indexOf(loc);
 
-swapPiece(indexLoc,target,cube_edges);
-swapPiece(indexLocMirror,targetMirror,cube_edges);
-}
-function isFinished(){
-	//if no difference between array and how it should be
-	if(differenceIndex(cube_edges,normal)==-1){return true;}
 
-	else{return false;}
+	return twists;
 }
-function checkFlip(array){
-	var flips="";
-		for (var i = 0; i < array.length; i++) {
-			var a  = array[i];
-			var b = mirror[i];
-			if(a === b && (a!=="c"&&a!=="e")){
-				flips += normal[i];
-				swapPiece(i,normal.indexOf(mirror[i]),cube_edges);
-			}
-		}
-		if(flips.length%2==1){
-			flips += "c";
-		}
-		console.log("Flips: "+flips);
-}
-function getEdgeSolution(){
-	var solution = "";
-	var shootLoc;
-	var prev="";
-	var prevCE="";
-	checkFlip(cube_edges);
-	for(var i = 0; i< 20; i++){
-		shootLoc = getEdgeBufferPiece();
-		if(shootLoc === "c" || shootLoc === "e" ){
-			prevCE = shootLoc;
-			if(prev!=""){
-				if(prevCE == shootLoc);
-				solution += normal[mirror.indexOf(prev)];
-			}
-			else{
-				solution += prev;
+function twistPiece(piece,CW){
 
-			}
-			if(isFinished()){
-				return solution;
-			}
-			//buffer is in place
-			shootLoc = findNewBuffer(cube_edges);
-			swap(shootLoc);
-			shootLoc = getEdgeBufferPiece();
-			prev = shootLoc;
-		}
-		else if(prev===shootLoc || mirror[normal.indexOf(prev)] === shootLoc){
-			if(isFinished()){
-				return solution;
-			}
-		}
-		solution += shootLoc;
-		swap(shootLoc);
+	var temp = normal.indexOf(piece);
+	var a= cw_corners.indexOf(piece);
+  var b1 = normal[a];
+	var b = cw_corners.indexOf(b1);
+if(CW){
+	swapSingleCorner(temp,a);
+	swapSingleCorner(temp,b);
 	}
-	return solution;
+else{
+		swapSingleCorner(temp,b);
+		swapSingleCorner(temp,a);
+		}
+
+
+}
+
+function swapCorner(piece) {
+  var a = normal.indexOf(piece);
+	swapSingleCorner(0,a)
+  //swap mirror piece with location it belongs;
+  a = normal.indexOf(cw_corners[a]);
+	swapSingleCorner(16,a);
+
+	 a = normal.indexOf(cw_corners[a]);
+	swapSingleCorner(13,a);
+
+
+}
+function swapSingleCorner(a,b){
+	var temp = solution_corners[a];
+  solution_corners[a] = solution_corners[b];
+  solution_corners[b] = temp;
+}
+function findWrongCorner() {
+  for (var i = 0; i < normal.length; i++) {
+    if (normal[i] != solution_corners[i]) {
+			if(normal[i]!="a" && normal[i]!="n"&& normal[i]!="r"){
+      return normal[i];
+
+    }
+  }
+}
+  return "NONE";
+
+}
+function countWrongCorners() {
+  var count = 0;
+  for (var i = 0; i < normal.length; i++) {
+    if (normal[i] != solution_corners[i]) {
+      count ++;
+
+  }
+}
+  return count;
+
+}
+
+
+function getEdgeSolution() {
+  var result = "";
+  var eCycles =0;
+  solution_edges = [...cube_edges];
+  var i = 0;
+  //First get Flips
+   var flips = getFlips();
+  //Then solve rest of the cube
+  while (i < 30 && !edgesSolved()) {
+    var piece = getEdgeBuffer();
+    i++;
+    if (piece == "c" || piece == "e") {
+      var piece = findWrongEdge();
+			if(piece=="NONE"){
+				flipPiece("c");
+				if(edgesSolved()){
+					break;
+				}
+			}
+      eCycles ++;
+		}
+      result += piece;
+      swapEdge(piece);
+
+
+    //swap piece with location it belongs;
+
+  }
+  result = cleanSolution(result);
+  if(flips.length>0){
+  result += "("+cleanSolution(flips)+")";
+  }
+  console.log("Edge cycles:" + eCycles);
+  return result;
+}
+
+function getFlips(){
+	var flips = ""
+	for(var i = 0; i< solution_edges.length; i++){
+		if(solution_edges[i]==mirror_edges[i]){
+			flips += solution_edges[i];
+			flipPiece(solution_edges[i]);
+
+		}
+	}
+
+	return flips;
+}
+
+function flipPiece(temp){
+	var i = mirror_edges.indexOf(temp);
+	var mir_i = normal.indexOf(temp)
+	swapSingleEdge(i,mir_i);
+}
+function swapEdge(piece) {
+  var a = normal.indexOf(piece);
+	swapSingleEdge(2,a)
+  //swap mirror piece with location it belongs;
+  var a_mir = mirror_edges.indexOf(piece);
+	swapSingleEdge(4,a_mir);
+
+
+}
+function swapSingleEdge(a,b){
+	var temp = solution_edges[a];
+  solution_edges[a] = solution_edges[b];
+  solution_edges[b] = temp;
+}
+function findWrongEdge() {
+  for (var i = 0; i < normal.length; i++) {
+    if (normal[i] != solution_edges[i]) {
+			if(normal[i]!="c" && normal[i]!="e"){
+      return normal[i];
+
+    }
+  }
+}
+  return "NONE";
+
 }
--- exec-trainer/js/main.js
+++ exec-trainer/js/main.js
@@ -1,43 +1,118 @@
  function init() {
-        var textbox = document.querySelector("#contents");
-		var box2 = document.querySelector("#content-text");
-		var scramble = getScramble(3);
-            box2.innerHTML= scramble;
-			convert(scramble);
-
-        // Add click event listener
-        textbox.addEventListener("click", function() {
-            var box = document.querySelector("#content-text");
-			var scramble = getScramble(3);
-            box.innerHTML= scramble;
-			convert(scramble);
-
-        });
-    }
-$(document).ready(function() {init();});
+  newScramble();
 
 
+ }
+ $(document).ready(function() {
+   init();
+ });
+
+
+function newScramble(){
+  var box = document.querySelector("#content-text");
+  var scramble = getScramble(0);
+  box.innerHTML = scramble;
+  convert(scramble);
+}
+
+
+ function convert(scramble) {
+   //scramble ="B F";
+   scramble = clean(scramble);
+   console.log(scramble);
+   executeScramble(scramble);
+   var edge = document.querySelector("#edges");
+  edge.innerHTML = getEdgeSolution();
+  var corner = document.querySelector("#corners");
+ corner.innerHTML =getCornerSolution();
+   drawCube();
+
+ }
+
+ function clean(scramble) {
+   //split string to array
+   var scram_s = scramble.split(" ");
+   //remove empty strings
+   var scram = [];
+   for (var i = 0; i < scram_s.length; i++) {
+     if (scram_s[i] != "") {
+       scram.push(scram_s[i]);
+     }
+   }
+
+   return scram;
+ }
+
+ var cube_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+ var backup = ["u", "g", "i", "z", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "z"];
+
+ function drawCube() {
+   var c = document.getElementById("canvas");
+   var ctx = c.getContext("2d");
+   var size = Math.min(c.width / 12, c.height / 9);
+   drawCenters(ctx, size);
+   drawCorners(ctx, size);
+   drawEdges(ctx, size);
 
 
 
-	function convert(scramble){
-		scramble = clean(scramble);
-		console.log(scramble);
-		executeScramble(scramble);
-		getBlindSolution();
+ }
+
+ function drawEdges(ctx, size) {
+   var colors = getCubeColor(getCubeEdges());
+   //TOP FACE
+   ctx.strokeStyle = "#141414";
+   for (var i = 0; i < backup.length; i++) {
+     ctx.fillStyle = colors[i];
+     var x = i%4;
+     var y = Math.floor(i/4);
+     var z = (y == 0)? 1 : y % 4 ;
+     var z2 = Math.ceil((y%5)/5)+Math.floor(y/5)*2;
+     if (i%4== 2) {
+       ctx.fillRect(z * 3 * size + size, z2 *3* size +size * (Math.floor(x / 3) + x % 3), size, size);
+       ctx.strokeRect(z * 3 * size + size, z2 *3* size +size * (Math.floor(x / 3) +x % 3), size, size);
+
+     } else {
+       ctx.fillRect(z * 3 * size + ((x + 1) % 4) * size, z2 *3* size +size * (Math.floor(x / 3) + x % 3), size, size);
+       ctx.strokeRect(z * 3 * size + ((x + 1) % 4) * size, z2 *3* size +size * (Math.floor(x / 3) + x % 3), size, size);
+
+     }
+   }
+  }
 
 
-	}
-	function clean(scramble){
-		//split string to array
-		var scram_s =scramble.split(" ");
-		//remove empty strings
-		var scram = [];
-		for( var i = 0; i<scram_s.length; i++ ){
-				if(scram_s[i]!=""){
-				scram.push(scram_s[i]);
-			}
-		}
+ function drawCorners(ctx, size) {
+   var colors = getCubeColor(getCubeCorners());
+   ctx.strokeStyle = "#141414";
+   for (var i = 0; i < 24; i++) {
+     var x = i%4;
+     var y = Math.floor(i/4);
+     var z = (y == 0)? 1 : y % 4 ;
+     var z2 = Math.ceil((y%5)/5)+Math.floor(y/5)*2;
+     ctx.fillStyle = colors[i];
+     ctx.fillRect(z*3 * size + 2 * size * Math.ceil((x % 3) / 3), 3*z2*size +2 * size * Math.floor(x / 2), size, size);
+     ctx.strokeRect(z*3 * size + 2 * size * Math.ceil((x % 3) / 3), 3*z2*size +2 * size * Math.floor(x / 2), size, size);
 
-		return scram
-	}
+   }
+ }
+ function drawCenters(ctx, size) {
+   ctx.strokeStyle = "#141414";
+   ctx.fillStyle = "white";
+   ctx.fillRect(4*size,1*size, size, size);
+   ctx.strokeRect(4*size,1*size, size, size);
+   ctx.fillStyle = "green";
+   ctx.fillRect(4*size,4*size, size, size);
+   ctx.strokeRect(4*size,4*size, size, size);
+   ctx.fillStyle = "red";
+   ctx.fillRect(7*size,4*size, size, size);
+   ctx.strokeRect(7*size,4*size, size, size);
+   ctx.fillStyle = "blue";
+   ctx.fillRect(10*size,4*size, size, size);
+   ctx.strokeRect(10*size,4*size, size, size);
+   ctx.fillStyle = "orange";
+   ctx.fillRect(1*size,4*size, size, size);
+   ctx.strokeRect(1*size,4*size, size, size);
+   ctx.fillStyle = "yellow";
+   ctx.fillRect(4*size,7*size, size, size);
+   ctx.strokeRect(4*size,7*size, size, size);
+   }
